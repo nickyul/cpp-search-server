e7fe8fecd1abcbed6020efbe188acd47c9ce5a30
nickyul <156240952+nickyul@users.noreply.github.com>
2024-03-19 09:33:50 +0300

    accumulate in ComputeAverageRating

--- search-server/search_server.cpp
+++ search-server/search_server.cpp
@@ -91,11 +91,7 @@ int SearchServer::ComputeAverageRating(const std::vector<int>& ratings) {
     if (ratings.empty()) {
         return 0;
     }
-    int rating_sum = 0;
-    for (const int rating : ratings) {
-        rating_sum += rating;
-    }
-    return rating_sum / static_cast<int>(ratings.size());
+    return std::accumulate(ratings.begin(), ratings.end(), 0) / static_cast<int>(ratings.size());
 }
 
 
