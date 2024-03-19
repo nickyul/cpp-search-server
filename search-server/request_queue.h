46e372a5746a4348cc166037a068690765c3920c
nickyul <156240952+nickyul@users.noreply.github.com>
2024-03-19 09:32:30 +0300

    Decomp template methods

--- search-server/request_queue.h
+++ search-server/request_queue.h
@@ -7,21 +7,7 @@ public:
     explicit RequestQueue(const SearchServer& search_server);
 
     template <typename DocumentPredicate>
-    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
-
-        auto result_of_request = search_server_.FindTopDocuments(raw_query, document_predicate);
-        if (requests_.size() >= min_in_day_) {
-            if (requests_[0].result_of_request.size() != 0) {
-                --count_no_result_request;
-                requests_.pop_back();
-            }
-        }
-        if (result_of_request.empty()) {
-            ++count_no_result_request;
-        }
-        requests_.push_front({ result_of_request });
-        return result_of_request;
-    }
+    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate);
 
     std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status = DocumentStatus::ACTUAL) {
         return AddFindRequest(raw_query, [status](int document_id, DocumentStatus document_status, int rating) {return document_status == status; });
@@ -38,3 +24,20 @@ private:
     const static int min_in_day_ = 1440;
     int count_no_result_request = 0;
 };
+
+template <typename DocumentPredicate>
+std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
+
+    auto result_of_request = search_server_.FindTopDocuments(raw_query, document_predicate);
+    if (requests_.size() >= min_in_day_) {
+        if (requests_[0].result_of_request.size() != 0) {
+            --count_no_result_request;
+            requests_.pop_back();
+        }
+    }
+    if (result_of_request.empty()) {
+        ++count_no_result_request;
+    }
+    requests_.push_front({ result_of_request });
+    return result_of_request;
+}
